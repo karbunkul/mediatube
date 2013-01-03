a9588b2da89caaafdd28cdaaf1e7e343c6ffcd2b
karbunkul <karbunkul@yourtask.ru>
2013-01-04 03:04:11 +0600

    Add local task, fix mediatube-dashboard.tpl.php

--- template/mediatube-dashboard.tpl.php
+++ template/mediatube-dashboard.tpl.php
@@ -1,10 +1,9 @@
 <div class="well">
 <p>
- <a href="playlists">Плейлисты</a>
- <a href="1">Ролик 1</a>
- <a href="2">Ролик 2</a>
- <a href="3">Ролик 3</a>
- <a href="4">Ролик 4</a>
+ <a href="?q=<?php print $_GET['q']; ?>/1">Ролик 1</a>
+ <a href="?q=<?php print $_GET['q']; ?>/2">Ролик 2</a>
+ <a href="?q=<?php print $_GET['q']; ?>/3">Ролик 3</a>
+ <a href="?q=<?php print $_GET['q']; ?>/4">Ролик 4</a>
 	<p>123; <?php print $url;?>
 </div>
 
