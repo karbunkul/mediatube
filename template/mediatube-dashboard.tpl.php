a7c219dc8bfc750f14c4c682addd5dd1f88e6417
karbunkul <karbunkul@yourtask.ru>
2013-01-03 15:07:35 +0600

    First upload

---
+++ template/mediatube-dashboard.tpl.php
@@ -0,0 +1,11 @@
+<div class="well">
+<p>
+<a href="/tv/playlists">Плейлисты</a>
+<a href="/tv/1">Ролик 1</a>
+<a href="/tv/2">Ролик 2</a>
+<a href="/tv/3">Ролик 3</a>
+<a href="/tv/4">Ролик 4</a>
+	<p>123; <?php print $url;?>
+</div>
+
+<p>привет <?php print $hello;?>
