cd4e41d075600e2bc5506054151b81f4890454c4
karbunkul <karbunkul@yourtask.ru>
2013-01-03 17:05:44 +0600

    some fix

--- template/mediatube-dashboard.tpl.php
+++ template/mediatube-dashboard.tpl.php
@@ -1,10 +1,10 @@
 <div class="well">
 <p>
- <a href="/tv/playlists">Плейлисты</a>
- <a href="/tv/1">Ролик 1</a>
- <a href="/tv/2">Ролик 2</a>
- <a href="/tv/3">Ролик 3</a>
- <a href="/tv/4">Ролик 4</a>
+ <a href="playlists">Плейлисты</a>
+ <a href="1">Ролик 1</a>
+ <a href="2">Ролик 2</a>
+ <a href="3">Ролик 3</a>
+ <a href="4">Ролик 4</a>
 	<p>123; <?php print $url;?>
 </div>
 
