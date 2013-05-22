28a134e66fa0cc5bbef1cb7ce9f535a640884e09
karbunkul <karbunkul@yourtask.ru>
2013-05-22 17:20:29 +0600

    Изменения

--- template/mediatube-playlist.tpl.php
+++ template/mediatube-playlist.tpl.php
@@ -1,6 +1,6 @@
 
 <div class="well">
-<?php dsm(menu_get_menus()); foreach($playlist AS $item): ?>
+<?php dsm($playlist); foreach($playlist AS $item): ?>
 
 <div>
     <a href="<?php print $item['url']; ?>"><?php print $item['title']; ?></a>
--- template/mediatube-video.tpl.php
+++ template/mediatube-video.tpl.php
@@ -1,11 +1,11 @@
 
-<div class="well"> <?php print $movie['description']; ?> </div>
+<div class="well"> <?php dsm($movie); print $movie['description']; ?> </div>
 <center><div id="mediatube-container">Loading the player ...</div></center>
 
 <script type="text/javascript">
     jwplayer("mediatube-container").setup({
         autostart: "TRUE",
-        file: "<?php print $movie['source']; ?>",
+        file: "<?php print $movie['data']; ?>",
         height: 480,
         width: 640
     });
